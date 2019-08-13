c4da3638b2ea6b97e557c52cc7a10c11211d576a
Sveta <svet.crow@gmail.com>
2019-08-13 12:19:30 +0100

    menu toggle

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /*jQuery(document).ready(function(){});*/
 
+$('#menu').click(function() {
+     $('#menu-dropdown-content').toggle();
+  });
+
+$('#language').click(function() {
+     $('#lang-dropdown-content').toggle();
+  });
