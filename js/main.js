2c4c948e6fd3a9f4fa9a760dca63c6534f0189a8
Sveta <svet.crow@gmail.com>
2019-08-13 12:29:22 +0100

    toggle

--- js/main.js
+++ js/main.js
@@ -1,9 +1,45 @@
 /*jQuery(document).ready(function(){});*/
 
-$('#menu').click(function() {
+/*$('#menu').click(function() {
      $('#menu-dropdown-content').toggle();
   });
 
 $('#language').click(function() {
      $('#lang-dropdown-content').toggle();
-  });
+  });*/
+
+$(document).ready(function(){
+
+
+$("#menu").on('click', function(event){
+ event.stopPropagation();
+ $("#menu-dropdown-content").toggle();
+
+});
+
+
+$(document).on("click", function(event){
+event.stopPropagation();
+$("#menu-dropdown-content").hide();      
+
+});
+
+});
+
+$(document).ready(function(){
+
+
+$("#language").on('click', function(event){
+ event.stopPropagation();
+ $("#lang-dropdown-content").toggle();
+
+});
+
+
+$(document).on("click", function(event){
+event.stopPropagation();
+$("#lang-dropdown-content").hide();      
+
+});
+
+});
