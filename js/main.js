143e061342b776dbee66b0422b51bbc674ca28c6
Sveta <svet.crow@gmail.com>
2019-08-31 16:13:41 +0100

    fixed jvscript

--- js/main.js
+++ js/main.js
@@ -45,4 +45,29 @@ $("#lang-dropdown-content").hide();
 });
 
 
+$(document).ready(function(){
+        
+        
+        $('#contactform').validate({
+            submitHandler: function(form) {
+                
+                var form=$('#contactform').serialize();
+                var url=$('#contactform').attr('action');
+                
+                $('#overlay').show();
+                
+                $.ajax({
+                    type: 'post',
+                    url: url,
+                    data: form,
+                    
+                    success: function(responce){
+                       $('#contactform').trigger("reset"); 
+                        $('#overlay').hide();
+                    }
+                })
+                
+            }
+        });
+    });
  
