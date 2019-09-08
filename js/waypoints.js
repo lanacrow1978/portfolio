ab1833509a9a41811b4ce970a1030a74fd667641
Sveta <svet.crow@gmail.com>
2019-09-08 21:53:53 +0100

    slidein

--- js/waypoints.js
+++ js/waypoints.js
@@ -1,3 +1,6 @@
+// fading in Skills section on scroll
+
+
 var $fadein = $(".fadein");
 
 $fadein.waypoint(function(direction){
@@ -7,4 +10,36 @@ $fadein.waypoint(function(direction){
       $fadein.removeClass('fadein-waypoint');      
         }
 }, {offset: '50%'});
+
+
+//sliding in the tech skills diagram
+
+$(document).ready(function(){
+
+var $slideInLeft = $(".slidein-left");
+
+$slideInLeft.waypoint(function(direction){
+    if (direction == 'down'){
+    $slideInLeft.addClass('diagram-animate-left');
+        } else {
+      $slideInLeft.removeClass('diagram-animate-left');      
+        }
+}, {offset: '70%'});
+
+
+
+// sliding in the languages diagram
+
+var $slideInRight = $(".slidein-right");
+
+$slideInRight.waypoint(function(direction){
+    if (direction == 'down'){
+    $slideInRight.addClass('diagram-animate-right');
+        } else {
+      $slideInRight.removeClass('diagram-animate-right');      
+        }
+}, {offset: '80%'});
+
+});
+
 
