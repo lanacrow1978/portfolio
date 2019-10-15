faa11b5b68c4cbb5c6b63f381b661d1fa677dda8
Sveta <svet.crow@gmail.com>
2019-10-15 16:01:40 +0100

    created new Dorothy design

--- js/main.js
+++ js/main.js
@@ -106,6 +106,64 @@ $(document).ready(function() {
     })
 });
 
+// fading in portfolio section on scroll
+
+$(document).ready(function(){
+var $fadein = $(".fadein");
+
+$fadein.waypoint(function(direction){
+    if (direction == 'down'){
+    $fadein.addClass('fadein-waypoint');
+        } else {
+      $fadein.removeClass('fadein-waypoint');      
+        }
+}, {offset: '50%'});
+});
+
+// jumping skill icons
+
+$(document).ready(function(){
+var $move = $(".movement");
+
+$move.waypoint(function(direction){
+    if (direction == 'down'){
+    $move.addClass('movement-waypoint');
+        } else {
+      $move.removeClass('movement-waypoint');      
+        }
+}, {offset: '60%'});
+});
+
+//sliding in the tech skills diagram
+
+$(document).ready(function(){
+
+var $slideInLeft = $(".slidein-left");
+
+$slideInLeft.waypoint(function(direction){
+    if (direction == 'down'){
+    $slideInLeft.addClass('diagram-animate-left');
+        } else {
+      $slideInLeft.removeClass('diagram-animate-left');      
+        }
+}, {offset: '50%'});
+
+// sliding in the languages diagram
+
+var $slideInRight = $(".slidein-right");
+
+$slideInRight.waypoint(function(direction){
+    if (direction == 'down'){
+    $slideInRight.addClass('diagram-animate-right');
+        } else {
+      $slideInRight.removeClass('diagram-animate-right');      
+        }
+}, {offset: '50%'});
+
+});
+
+
+
 
 
 
